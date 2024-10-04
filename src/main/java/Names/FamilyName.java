ab56cac5eb6b8d53f0369e7009e5e6a3bcb7c53a
Johnrobertparadiang <treborparadiang3@gmail.com>
2024-10-04 19:44:11 +0800

    DeletingTest

---
+++ src/main/java/Names/FamilyName.java
@@ -0,0 +1,12 @@
+package Names;
+
+public class FamilyName {
+    public static void main(String[] args) {
+        System.out.println("JOHN ROBERT B. PARADIANG");
+        System.out.println("MARK RYAN B. PARADIANG");
+        System.out.println("PHILIP ANDREW B. PARADIANG");
+        System.out.println("EDNA B. PARADIANG");
+        System.out.println("MARVIN B. PARADIANG");
+
+    }
+}
